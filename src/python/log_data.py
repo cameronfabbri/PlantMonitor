3ba24e89f2946c1a0e395a27026b220e903855b7
Cameron Fabbri <cameronfabbri@gmail.com>
2018-11-23 23:43:52 -0600

    adding files

--- src/python/log_data.py
+++ src/python/log_data.py
@@ -3,25 +3,25 @@ import datetime
 
 if __name__ == '__main__':
 
-    f = open('sensors.txt','a')
+    with open('../../data/sensors.txt','a') as f:
+        while True:
+            s = serial.Serial('/dev/ttyACM0',9600)
+            vals = s.readline().rstrip().split(',')
 
-    while True:
-        s = serial.Serial('/dev/ttyACM0',9600)
-        vals = s.readline().rstrip().split(',')
+            try:
+                temp = vals[0]
+                humidity = vals[1]
+                light = vals[2]
+                moisture = vals[3]
+            except:
+                continue
 
-        try:
-            temp = vals[0]
-            humidity = vals[1]
-            light = vals[2]
-            moisture = vals[3]
-        except:
-            continue
-
-        now = datetime.datetime.now()
-        year = str(now.year)
-        month = str(now.month)
-        day = str(now.day)
-        hour = str(now.hour)
-        minute = str(now.minute)
-        second = str(now.second)
-        print temp+','+humidity+','+light+','+moisture+','+year+','+month+','+day+','+hour+','+minute+','+second+'\n'
+            now = datetime.datetime.now()
+            year = str(now.year)
+            month = str(now.month)
+            day = str(now.day)
+            hour = str(now.hour)
+            minute = str(now.minute)
+            second = str(now.second)
+            f.write(temp+','+humidity+','+light+','+moisture+','+year+','+month+','+day+','+hour+','+minute+','+second+'\n')
+            f.flush()
