a02f95e8501c9bfa3da5540a26206bb07f0dcb12
Cameron Fabbri <cameronfabbri@gmail.com>
2018-12-21 11:18:13 -0600

    update

--- data/avg.py
+++ data/avg.py
@@ -2,12 +2,14 @@ import numpy as np
 import sys
 
 total_m = []
-with open('sensors.txt', 'r') as f:
+with open('sensors.csv', 'r') as f:
     for line in f:
+        print(line.rstrip().split(',')[3])
+        exit()
         moisture = float(line.rstrip().split(',')[3])
         total_m.append(moisture)
 
 total_m = np.asarray(total_m)
 tot = len(total_m)
 #print np.mean(total_m[tot-int(sys.argv[1]):])
-print np.mean(total_m[:100])
+print(np.mean(total_m[:100]))
